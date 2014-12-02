c7adff13b91abeaed63c2a1ff86044709789a205
andy <andylei@gmail.com>
2014-12-01 22:02:19 -0500

    api

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-routing-simulator
-=================
+# routing-simulator
+
+## rest api
+
+### /users
+
+```json
+{
+  "id": "andy"
+}
+```
+
+- `GET /users`
+
+- `GET /users/{id}`
+
+- `POST /users`
+
+### /connections
+
+```json
+{
+  "from": "andy",
+  "to": "bob
+}
+```
+
+- `GET /connections?related-to={user_id}`
+
+- `POST /connections`
+
+### /messages
+
+```json
+{
+  "body": "hi bob",
+  "to": "andy",
+  "from": "bob
+}
+```
+
+- `POST /messages`
+
+- `GET /users/{id}/messages`
+
+- `GET /users/{id}/sent-messages`
+
--- index.js
+++ index.js
@@ -1,11 +1,150 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('lodash');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+
+// ============  data  =============
+
+function ConnectionStore() {
+  this.data = {};
+}
+ConnectionStore.prototype.list = function(u) {
+  return _.keys(this.data[u]);
+};
+ConnectionStore.prototype.contains = function(u1, u2) {
+  return this.data[u1] && this.data[u1][u2];
+};
+ConnectionStore.prototype.add = function(u1, u2) {
+  if (!this.data[u1]) {
+    this.data[u1] = {};
+  }
+  if (!this.data[u2]) {
+    this.data[u2] = {};
+  }
+  this.data[u1][u2] = true;
+  this.data[u2][u1] = true;
+};
+
+function MessageStore() {
+  this.data = [];
+}
+MessageStore.prototype.getTo = function(user) {
+  return _.filter(this.data, {to: user});
+};
+MessageStore.prototype.getFrom = function(user) {
+  return _.filter(this.data, {from: user});
+};
+MessageStore.prototype.push = function(msg) {
+  msg = _.pick(msg, ['body', 'from', 'to']);
+  this.data.push(msg);
+  return msg;
+};
+
+var USERS = {};
+var CONNECTIONS = new ConnectionStore();
+var MESSAGES = new MessageStore();
+
+
+// ========= helpers ==========
+
+function buildUser(name) {
+  return {id: name};
+}
+
+function buildConnection(from, to) {
+  return {from: from, to: to};
+}
+
+function isValidMessage(msg) {
+  return msg.body &&
+    USERS[msg.to] &&
+    USERS[msg.from] &&
+    CONNECTIONS.contains(msg.from, msg.to);
+}
+
+// ========= routes ===========
+
+app.get('/users', function(req, res) {
+  res.send(_.keys(USERS).map(buildUser));
+});
+
+app.get('/users/:id', function(req, res) {
+  var name = req.params.id;
+  if (USERS[name]) {
+    res.send(buildUser(name));
+  } else {
+    res.status(404).send('Not Found');
+  }
+});
+
+app.post('/users', function(req, res) {
+  var name = req.body.id;
+  if (name) {
+    if (USERS[name]) {
+      res.status(409).send('That user already exists');
+    } else {
+      USERS[name] = true;
+      res.send(buildUser(name));
+    }
+  } else {
+    res.status(400).send('Must include user id');
+  }
+});
+
+
+app.get('/connections', function(req, res) {
+  var rel = req.query['related-to'];
+  if (rel && USERS[rel]) {
+    res.send(CONNECTIONS.list(rel));
+  } else {
+    res.status(400).send('Bad query param related-to');
+  }
+});
+
+app.post('/connections', function(req, res) {
+  var conn = req.body;
+  if (CONNECTIONS.contains(conn.from, conn.to)) {
+    res.status(409).send('That connection already exists');
+  } else if (USERS[conn.from] && USERS[conn.to]) {
+    CONNECTIONS.add(conn.from, conn.to);
+    res.send(buildConnection(conn.from, conn.to));
+  } else {
+    res.status(400).send('Invalid connection');
+  }
+});
+
+
+app.post('/messages', function(req, res) {
+  var msg = req.body;
+  if (isValidMessage(msg)) {
+    res.send(MESSAGES.push(msg));
+  } else {
+    res.status(400).send('Bad message');
+  }
+});
+
+app.get('/users/:id/messages', function(req, res) {
+  var name = req.params.id;
+  if (USERS[name]) {
+    res.send(MESSAGES.getTo(name));
+  } else {
+    res.status(404).send('Not Found');
+  }
+});
+
+app.get('/users/:id/sent-messages', function(req, res) {
+  var name = req.params.id;
+  if (USERS[name]) {
+    res.send(MESSAGES.getFrom(name));
+  } else {
+    res.status(404).send('Not Found');
+  }
 });
 
 app.listen(app.get('port'), function() {
